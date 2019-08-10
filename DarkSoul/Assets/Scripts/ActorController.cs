150ca51d2041c4ee6fd77ae709b6548019a28d56
yhccc <yinhc0129@163.com>
2019-08-10 11:22:30 +0800

    8.10commit(Jump)

--- DarkSoul/Assets/Scripts/ActorController.cs
+++ DarkSoul/Assets/Scripts/ActorController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 /// <summary>
 /// 
 /// </summary>
-public class ActorController : MonoBehaviour {
+public class ActorController : MonoBehaviour
+{
 
     public GameObject model;
-    public float walkSpeed = 2.0f;//角色移动速度
+    public float walkSpeed = 1.4f;//角色移动速度
+    public float runSpeed = 2.8f;
 
     private PlayerInput playerInput;
     private Animator anim;
@@ -25,21 +27,31 @@ public class ActorController : MonoBehaviour {
     private void Update()
     {
         //控制动画播放和角色转向
-        anim.SetFloat("forward", playerInput.Dmagnitude);
+
+        //动画播放
+        anim.SetFloat("forward", playerInput.Dmagnitude *
+            Mathf.Lerp(anim.GetFloat("forward"), playerInput.run ? 2.0f : 1.0f, 0.5f));//动画过渡效果
+        if (playerInput.jump)
+            anim.SetTrigger("jump");
+
         //防止停止按键后，Dup和Dright归0，forward被设置为0，转回原来方向
+        //角色转向
         if (playerInput.Dmagnitude > 0.1f)
         {
-            model.transform.forward = playerInput.Ddirection;
+            model.transform.forward =
+                Vector3.Slerp(model.transform.forward, playerInput.Ddirection, 0.3f);//转身过渡效果
         }
-        movingVec = model.transform.forward * playerInput.Dmagnitude * walkSpeed;
-        //Bug：斜向移动时playerInput.Dmagnitude最大为根号2,直行时playerInput.Dmagnitude最大为1,造成移动速度不一致
+
+        //角色移动向量
+        movingVec = (model.transform.forward * playerInput.Dmagnitude * walkSpeed)
+            * (playerInput.run ? runSpeed : 1.0f);//跑步为2，行走为1
     }
 
     private void FixedUpdate()
     {
         //两种让角色移动的方式：
         //rigid.position += movingVec * Time.fixedDeltaTime;//设置位置=速度*时间
-        
+
         //rigid.velocity = movingVec;//设置速度，存在Bug：由于y分量为0，没有算入重力，当从高处下坡时会飞起来
         rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
     }
--- DarkSoul/Assets/Scripts/PlayerInput.cs
+++ DarkSoul/Assets/Scripts/PlayerInput.cs
@@ -8,16 +8,30 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour {
 
     //Variable
-    public string keyUp = "w";
-    public string keyDown = "s";
-    public string keyLeft = "a";
-    public string keyRight = "d";
+    [Header("----- Input Key Settings -----")]
+    public KeyCode keyUp = KeyCode.W;
+    public KeyCode keyDown = KeyCode.S;
+    public KeyCode keyLeft = KeyCode.A;
+    public KeyCode keyRight = KeyCode.D;
+           
+    public KeyCode keyA = KeyCode.LeftShift;//对应跑步
+    public KeyCode keyB = KeyCode.Space;//对应跳跃
+    public KeyCode keyC;
+    public KeyCode keyD;
 
+    [Header("----- Output Signals -----")]
     public float Dup;
     public float Dright;
     public float Dmagnitude;
     public Vector3 Ddirection;
 
+    //1.pressing signal
+    public bool run;//是否跑步
+    //2.trigger once signal
+    public bool jump;
+    //3.double trigger
+
+    [Header("----- Component Activate -----")]
     public bool inputEnable = true;
 
     private float targetDup;
@@ -25,6 +39,10 @@ public class PlayerInput : MonoBehaviour {
 
     private float velocityDup;
     private float velocityDright;
+
+    private Vector2 tempDAxis;
+    private float tempDup;
+    private float tempDright;
     private void Update()
     {
         //将wasd的按下转为（-1，1）间的一个信号
@@ -41,10 +59,32 @@ public class PlayerInput : MonoBehaviour {
         //根据wasd按下的程度设置值（存在过渡过程）
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+        //解决Bug：斜向移动时playerInput.Dmagnitude最大为根号2,直行时playerInput.Dmagnitude最大为1,造成移动速度不一致
+        tempDAxis = SquareToCircle(new Vector2(Dright, Dup));
+        tempDup = tempDAxis.y;
+        tempDright = tempDAxis.x;
 
         //计算到原点之间的距离（适用于摇杆）
-        Dmagnitude = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        //Bug：斜向移动时playerInput.Dmagnitude最大为根号2,直行时playerInput.Dmagnitude最大为1,造成移动速度不一致
+        //Dmagnitude = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        Dmagnitude = Mathf.Sqrt((tempDup * tempDup) + (tempDright * tempDright));
         //计算前进的方向
-        Ddirection = Dright * transform.right + Dup * transform.forward;
+        Ddirection = tempDright * transform.right + tempDup * transform.forward;
+
+        run = Input.GetKey(keyA);
+        jump = Input.GetKeyDown(keyB);
+    }
+
+    /// <summary>
+    /// 将输入的方形坐标转成球形坐标
+    /// 解决Bug：斜向移动时playerInput.Dmagnitude最大为根号2,直行时playerInput.Dmagnitude最大为1,造成移动速度不一致
+    /// </summary>
+    /// <param name="input">输入坐标</param>
+    /// <returns>输出坐标</returns>
+    private Vector2 SquareToCircle(Vector2 input)
+    {
+        Vector2 output = new Vector2(input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2.0f),
+            input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2.0f));
+        return output;
     }
 }
