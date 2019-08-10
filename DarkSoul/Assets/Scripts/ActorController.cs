838698dacd9255eb7cc0642b013b83f99ce40386
yhccc <yinhc0129@163.com>
2019-08-10 10:03:19 +0800

    8.10commit(PlayerMove)

---
+++ DarkSoul/Assets/Scripts/ActorController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class ActorController : MonoBehaviour {
+
+    public GameObject model;
+    public float walkSpeed = 2.0f;//角色移动速度
+
+    private PlayerInput playerInput;
+    private Animator anim;
+    private Rigidbody rigid;
+    private Vector3 movingVec;//角色移动向量
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //控制动画播放和角色转向
+        anim.SetFloat("forward", playerInput.Dmagnitude);
+        //防止停止按键后，Dup和Dright归0，forward被设置为0，转回原来方向
+        if (playerInput.Dmagnitude > 0.1f)
+        {
+            model.transform.forward = playerInput.Ddirection;
+        }
+        movingVec = model.transform.forward * playerInput.Dmagnitude * walkSpeed;
+        //Bug：斜向移动时playerInput.Dmagnitude最大为根号2,直行时playerInput.Dmagnitude最大为1,造成移动速度不一致
+    }
+
+    private void FixedUpdate()
+    {
+        //两种让角色移动的方式：
+        //rigid.position += movingVec * Time.fixedDeltaTime;//设置位置=速度*时间
+        
+        //rigid.velocity = movingVec;//设置速度，存在Bug：由于y分量为0，没有算入重力，当从高处下坡时会飞起来
+        rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
+    }
+}
---
+++ DarkSoul/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class PlayerInput : MonoBehaviour {
+
+    //Variable
+    public string keyUp = "w";
+    public string keyDown = "s";
+    public string keyLeft = "a";
+    public string keyRight = "d";
+
+    public float Dup;
+    public float Dright;
+    public float Dmagnitude;
+    public Vector3 Ddirection;
+
+    public bool inputEnable = true;
+
+    private float targetDup;
+    private float targetDright;
+
+    private float velocityDup;
+    private float velocityDright;
+    private void Update()
+    {
+        //将wasd的按下转为（-1，1）间的一个信号
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0.0f) - (Input.GetKey(keyDown) ? 1.0f : 0.0f);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0.0f) - (Input.GetKey(keyLeft) ? 1.0f : 0.0f);
+        
+        //该脚本是否开启
+        if(!inputEnable)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+
+        //根据wasd按下的程度设置值（存在过渡过程）
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+
+        //计算到原点之间的距离（适用于摇杆）
+        Dmagnitude = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        //计算前进的方向
+        Ddirection = Dright * transform.right + Dup * transform.forward;
+    }
+}
